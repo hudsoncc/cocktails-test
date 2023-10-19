d72bc4c62e6ecb4b2598f9a48186eba279b3d01b
Hudson M <hudson@codecommittee.com>
2023-10-19 17:36:26 +0100

    New Xcode project

---
+++ Cocktails/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cocktails
+//
+//  Created by Hudson Maul on 19/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
